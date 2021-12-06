f40a5301dbb9c0f1e2456354b644f82c9a7742ed
Iago Moreira Bezerra <iagoejesus61@gmail.com>
2021-12-06 16:41:52 -0300

    correction-formatAmount

--- script.js
+++ script.js
@@ -141,8 +141,8 @@ const DOM = {
 
 const Utils = {
   formatAmount(value){
-    value = Number(value) * 100
-    return value
+    value *= 100
+    return Math.round(value)
   },
 
   formatDate(date){
